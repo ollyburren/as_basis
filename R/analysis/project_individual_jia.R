5646dfadb73bb47875fbaf8eb8fc9c5186fad28a
ollyburren <oliver.burren@cimr.cam.ac.uk>
2017-11-03 18:58:18 +0000

    Code to project jia individual data

--- R/analysis/project_individual_jia.R
+++ R/analysis/project_individual_jia.R
@@ -1,15 +1,46 @@
 ## ok given a genotype compute the expected posterior log(OR)
+#library(devtools)
+#install_github('ollyburren/cupcake')
 
-library(cupcake)
+library(optparse)
+
+option_list = list(
+        make_option(c("-o", "--out"), type="character", default=NULL,
+              help="output dir", metavar="character"),
+        make_option(c("-c", "--chromosome"), type="character",
+                    help="chromosome to parse", metavar="character"),
+        make_option(c("-s", "--samplefile"), type="character", default=NULL,
+                                help="List of samples to process", metavar="character")
+        )
+
+
+## This script processes RAW summary stats it uses a pregenerated list of
+## here we define the posterior lookup - this needs to be calculated for a given basis
+## see lor_lookup.R for more details
 support.dir<-'/scratch/ob219/as_basis/support_tab'
 lor.lu.file <- file.path(support.dir,'lor_posterior.tab')
+opt_parser = OptionParser(option_list=option_list);
+args = parse_args(opt_parser)
+#message(args)
+
+library(cupcake)
+
+## read in samples file
+#args <- list(
+#  sample.file = '/home/ob219/scratch/as_basis/jia_ind_analysis/splits/xaz',
+#  chr = '1',
+#  out = '/home/ob219/scratch/as_basis/jia_ind_analysis/ind_basis'
+#)
+samples <- scan(file=args$samplefile,"character()")
+
 dat <- fread(lor.lu.file,skip=1L)[,.(V1,V2,V3,V4)]
 setnames(dat,c('f','lor.00','lor.01','lor.11'))
 ## lets create a model for each of these
-
-library(ggplot2)
 colnames(dat) <- make.unique(colnames(dat))
 m <- reshape2::melt(dat,"f")
+
+
+#library(ggplot2)
 #ggplot(m[grep("%", m$variable,invert=TRUE),],
 #       aes(x=f,y=value,col=variable,group=variable)) + geom_point() + geom_smooth() + labs(x="AF",y="log OR")
 
@@ -26,22 +57,26 @@ names(mods) <- gtn
 ## next load in some genotype data
 
 gwas_data_dir <- '/home/ob219/scratch/as_basis/gwas_stats/input_files'
-ref_af_file<-file.path(support.dir,'as_basis_snps.tab')
+ref_af_file<-file.path(support.dir,'as_basis_snps_with_alleles.tab')
 basis.snps <- fread(ref_af_file)[,pid:=paste(chr,position,sep=':')]
 DTl <- split(basis.snps,basis.snps$chr)
 library(annotSnpStats)
 
 ## loop over each chromosome and create a matrix of expected log OR
-  chr <- 1
-  gt.file <- file.path('/scratch/wallace/JIA-2017-data/',sprintf('annotsnpstats-%d.RData',chr))
+  target.chr <- args$chr
+  gt.file <- file.path('/scratch/wallace/JIA-2017-data/',sprintf('annotsnpstats-%s.RData',target.chr))
+  message(sprintf("Loading %s",gt.file))
   X <- get(load(gt.file))
 
  gwas.snps <- data.table(snps(X))[,c('pid','order'):=list(paste(chromosome,position,sep=':'),1:.N),]
- snp.idx <- which(gwas.snps$pid %in% DTl[[chr]]$pid)
- if(length(snp.idx) != nrow(DTl[[chr]]))
+ snp.idx <- which(gwas.snps$pid %in% DTl[[target.chr]]$pid)
+ if(length(snp.idx) != nrow(DTl[[target.chr]]))
   stop("Not all SNPs in the basis")
  ## only care about controls
- sample.idx <- which(samples(X)$phenotype==1)
+ sample.idx <- which(samples(X)$ID_1 %in% samples)
+ if(length(sample.idx) != length(samples))
+  stop("Missing some some samples in gt file !")
+ message("Converting to snpMatrix object")
  sm<-as(X,"SnpMatrix")
  sm <- sm[sample.idx,snp.idx]
  ## This is super slow perhaps convert to a snpMatrix object ?
@@ -59,7 +94,28 @@ library(annotSnpStats)
  Xst[value=='01',lor:=lor.00]
  Xst[value=='02',lor:=lor.01]
  Xst[value=='03',lor:=lor.11]
- Xst[is.na(lor),]
+ if(any(is.na(Xst$lor))){
+   stop("Things don't join up aborting")
+ }
+ #Xst[is.na(lor),]
  Xst <- Xst[,.(sample,pid,lor)]
+
+ ref.DT <- subset(basis.snps,chr==target.chr)
+ DT<-gwas.snps[,.(chromosome,position,allele.1,allele.2)]
+ setnames(DT,c('chr','position','a1','a2'))
+
+ flip.idx <- flip_allele(DT,ref.DT)
+ Xst[,c('or','flipped'):=list(exp(lor),0)]
+ Xst[pid %in% DT[flip.idx,]$pid,c('or','flipped'):=list(exp(lor*-1),1)]
+ of <- sprintf("chr%s_%s.RDS",target.chr,basename(args$samplefile))
+ message(sprintf("Writing results to %s",of))
+ saveRDS(Xst[,.(sample,pid,or,flipped)],file=file.path(args$out,of))
+
+
+
+
+
+
+
 
  # also store the support file
